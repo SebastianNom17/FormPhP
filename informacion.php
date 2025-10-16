9e8aec3165c4f7ee454cb647888b75c587213b0e
Sebastián Nomesque <sebastiannom07@gmail.com>
2025-10-15 22:30:51 -0500

    Creación del archivo informacion.php para mostrar los datos del formulario

--- informacion.php
+++ informacion.php
@@ -3,8 +3,12 @@ $nombre = $_POST['nombre'];
 $edad = $_POST['edad'];
 $direccion = $_POST['direccion'];
 $salario = $_POST['salario'];
+$correo = $_POST['correo'];
+$cargo = $_POST['cargo'];
 
 echo "<h2>Información recibida</h2>";
 echo "El empleado $nombre tiene $edad años.<br>";
-echo "Vive en $direccion y su salario es: $salario.";
+echo "Vive en $direccion y su salario es: $salario.<br>";
+echo "Su correo electrónico es: $correo.<br>";
+echo "Y ocupa el cargo de: $cargo.";
 ?>
